f76d1bdbcdcd9d25669de770db1b52d0b0025e69
Jay Chan <jayc10@uci.edu>
2025-06-22 17:52:24 -0700

    image use / revamp, text with pedals done !

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,13 +9,51 @@ export default function App() {
         <h1 className="text-right">ELECTRIC | BASS</h1>
       </div>
       {/* Black Box (OMG TESTING??)*/}
-      <div className="w-full h-[73vh] bg-dark text-beige flex items-center justify-center mt-4">
-        <h1 className= "absolute text-right right-[1%] !text-[205px] tracking-wide z-0">RESONANCE</h1>
-        {/* fill in main content here */}
+      <div className="w-full h-[73vh] bg-dark text-beige flex items-center justify-center mt-4 relative">
+        <img
+          src="/src/assets/images/background.png"
+          alt="background image"
+          className="absolute left-0 h-full z-0"
+        />
+        {/* Pedals */}
+        <div className="absolute flex z-10 right-1/9 top-1/7 space-x-6">
+          <div className="flex flex-col items-center text-center space-y-1">
+            <p>acoustic</p>
+            <img
+              src="/src/assets/images/acoustic_pedal.png"
+              alt="acoustic pedal"
+            />
+            <p className="text-xs">CCV yung kai</p>
+          </div>
+          <div className="flex flex-col items-center text-center space-y-1 w-40">
+            <p>electric</p>
+            <img
+              src="/src/assets/images/electric_pedal.png"
+              alt="electric pedal"
+            />
+            <p className="text-xs">RF coalowl うるせー！ <br />reprint edition</p>
+          </div>
+          <div className="flex flex-col items-center text-center space-y-1">
+            <p>bass</p>
+            <img
+              src="/src/assets/images/bass_pedal.png"
+              alt="bass pedal"
+            />
+            <p className="text-xs">PPDS coalowl 押せ！引け！<br />reprint edition</p>
+          </div>
+          <div className="flex flex-col items-center text-center space-y-1">
+            <p>more</p>
+            <img
+              src="/src/assets/images/more_pedal.png"
+              alt="more pedal"
+            />
+            <p className="text-xs">DOMB might 日差し<br />reprint edition</p>
+          </div>
+        </div>
       </div>
       {/* Footers */}
       <div className="w-full flex justify-between pt-11">
-        <h1>JUN 2025</h1>
+        <h1>SUM 2025</h1>
         <h1>001</h1>
         <h1>
           <a
